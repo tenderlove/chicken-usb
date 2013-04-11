b22266e8286723fa74c426a6c5a8df8b78b2b955
Aaron Patterson <aaron.patterson@gmail.com>
2013-04-10 18:20:21 -0700

    wrapping busses

--- usb.scm
+++ usb.scm
@@ -23,14 +23,26 @@ EOF
 
 ;;; support routines
 
+(define-record-type usb-bus
+  (usb-wrap-bus context)
+  usb-bus?
+  (context usb-unwrap-bus))
+
 (define (usb-each-bus cb)
   (let loop ((bus (usb-first-bus)))
     (if bus (begin (cb bus) (loop (usb-next-bus bus))))))
 
-(define usb-next-bus
+(define (usb-next-bus bus)
+  (let ((bus (usb_bus->next (usb-unwrap-bus bus))))
+    (if bus (usb-wrap-bus bus))))
+
+(define usb-find-devices (foreign-lambda int "usb_find_devices"))
+(define (usb-first-bus) (usb-wrap-bus (usb_get_busses)))
+
+(define usb_bus->next
  (foreign-lambda*
    (c-pointer usb_bus) ((c-pointer bus))
    "C_return(((struct usb_bus *)bus)->next);\n"))
-(define usb-find-devices (foreign-lambda int "usb_find_devices"))
-(define usb-first-bus (foreign-lambda (c-pointer usb_bus) "usb_get_busses"))
+
+(define usb_get_busses (foreign-lambda (c-pointer usb_bus) "usb_get_busses"))
 )
